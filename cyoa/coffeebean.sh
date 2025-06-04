9d863cc907e8d455d1c733df00dcebeb4a19d544
catfacts117 <plights_phenols.0d@icloud.com>
2025-06-04 11:17:02 -0400

    Update coffeebean.sh

--- cyoa/coffeebean.sh
+++ cyoa/coffeebean.sh
@@ -26,7 +26,7 @@ echo
 echo
 echo
 
-type_out "a CYOA short story about enjoying a weekend with your feline friend"
+type_out 'a CYOA short story about enjoying a weekend with your feline friend'
 
 
 ### 1.0
@@ -37,16 +37,16 @@ echo
 echo
 sleep 2
 
-type_out "you wake up"
-type_out "the birds are chirping"
-type_out "it is Saturday morning"
-type_out "there is a cat on your face"
-type_out "you cant breathe"
-type_out "how do you react?"
+type_out 'you wake up'
+type_out 'the birds are chirping'
+type_out 'it is Saturday morning'
+type_out 'there is a cat on your face'
+type_out 'you cant breathe'
+type_out 'how do you react?'
 
 echo ------
-echo "1. go back to sleep"
-echo "2. move cat gently"
+echo '1. go back to sleep'
+echo '2. move cat gently'
 echo ------
 echo
 read choice
@@ -54,22 +54,27 @@ read choice
 case $choice in
 
     1)
-        type_out "the sound of purring helps you drift back to sleep."
-        type_out "you use the rest of the saturday strategically dreaming of pizza."
-        type_out "rest for resistance."
-        type_out "---the end.---"
+        type_out 'the sound of purring helps you drift back to sleep.'
+        type_out 'you use the rest of the saturday strategically dreaming of pizza.'
+        type_out 'rest for resistance.'
+        type_out '---the end.---'
         exit
         ;;
     
     2)
-        type_out "you attempt to pick up the cat"
-        type_out "the cat punishes you by swatting your hand"
-        type_out "then leaps off your face, digging its hind paws into your cheek"
-        type_out "you are awake"
+        type_out 'you attempt to pick up the cat'
+        type_out 'the cat punishes you by swatting your hand'
+        type_out 'then leaps off your face, digging its hind paws into your cheek'
+        type_out 'you are awake'
         ;;
 esac
 
 ### 1.1
+
+echo
+echo
+
+sleep 2
 
 type_out "you wake up"
 type_out "the birds are chirping"
